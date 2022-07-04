469b91211d7e4e189ad8eefa8625fc4e8986527e
eung7 <wannabeeung@gmail.com>
2022-07-05 00:32:15 +0900

    Implementing Webservice to Fetch News and Creating Models

---
+++ Stocks/Models/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  Stocks
+//
+//  Created by 김응철 on 2022/07/05.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    let title: String
+    let imageURL: String
+    let publication: String
+}
--- Stocks/Services/WebService.swift
+++ Stocks/Services/WebService.swift
@@ -8,6 +8,19 @@
 import Foundation
 
 class WebService {
+    func getTopNews(completion: @escaping ([Article]) -> Void) {
+        guard let url = URL(string: "https://island-bramble.glitch.me/top-news") else { return }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let articles = try? JSONDecoder().decode([Article].self, from: data)
+            DispatchQueue.main.async {
+                if let articles = articles {
+                    completion(articles)
+                }
+            }
+        }.resume()
+    }
+    
     func getStocks(completion: @escaping ([Stock]) -> Void) {
         guard let url = URL(string: "https://island-bramble.glitch.me/stocks") else { return }
         URLSession.shared.dataTask(with: url) { data, response, error in
