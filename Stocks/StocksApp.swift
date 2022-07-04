98e4c6668efdca4a96d7de9e86b9f6df244c76d9
eung7 <wannabeeung@gmail.com>
2022-07-04 20:11:40 +0900

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by 김응철 on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
